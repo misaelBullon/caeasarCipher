de590bd72d3e542e3cc7c352bb617d670b8313ae
misaelBullon <misa.esteban@gmail.com>
2020-11-09 12:08:55 -0300

    Modificación final del ejercicio

--- cipher.rb
+++ cipher.rb
@@ -7,16 +7,16 @@ puts "Ingrese la clave"
 clave = gets.chomp.to_i
 
 cadena.each_char do |char|
-    if char == " "
-        nuevaCadena<<" "
-    elsif 
-        index = alfabeto.index(char.downcase)
+    if alfabeto.include?(char)
+        index = alfabeto.index(char)
         nuevaCadena<<alfabeto[(index + clave)%26]
+    elsif alfabeto.include?(char.downcase)
+        index = alfabeto.index(char.downcase)
+        nuevaCadena<<alfabeto[(index + clave)%26].upcase
+    else    
+        nuevaCadena<<char
     end
-
 end
 
-puts  nuevaCadena
-
-
+puts nuevaCadena
 
